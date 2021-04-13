b59fe3a1e233985113617b62eee0881841e084e7
17 <17@DESKTOP-J9FO79P>
2021-04-13 09:20:38 +0900

    수정

--- Test2021-04-13/src/day11/SecondClass.java
+++ Test2021-04-13/src/day11/SecondClass.java
@@ -4,7 +4,7 @@ public class SecondClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		System.out.println("첫번째 클래스");
+		System.out.println("지우고 다시씀");
 
 	}
 
